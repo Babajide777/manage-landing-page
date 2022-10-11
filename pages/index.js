ac413fdebed62d91bee3dd688d857f6eedf59225
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-10-11 10:27:31 -0500

    layout for first component done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { hamLogo, logoIcon } from "../data/icons";
+import { hamLogo, illustrationLogo, logoIcon } from "../data/icons";
 
 export default function Home() {
   return (
@@ -11,21 +11,31 @@ export default function Home() {
       </Head>
       <nav className="flex justify-around h-28 items-center">
         <div className="logo">{logoIcon()}</div>
-
         <ul className="nav-links lg:flex hidden lg:block lg:justify-around lg:w-2/5 items-center font-medium text-[13px] leading-[18.99px] text-otherBlue ">
-          <li className="hover:opacity-50">Pricing</li>
-          <li className="hover:opacity-50">Product</li>
-          <li className="hover:opacity-50">About Us</li>
-          <li className="hover:opacity-50">Careers</li>
-          <li className="hover:opacity-50">Community</li>
+          <li className="hover:opacity-50 cursor-pointer">Pricing</li>
+          <li className="hover:opacity-50 cursor-pointer">Product</li>
+          <li className="hover:opacity-50 cursor-pointer">About Us</li>
+          <li className="hover:opacity-50 cursor-pointer">Careers</li>
+          <li className="hover:opacity-50 cursor-pointer">Community</li>
         </ul>
-
         <button className="started lg:flex items-center hover:opacity-50 justify-center hidden lg:block bg-otherOrange font-bold text-[13px] leading-[18.99px] w-[137px] h-[44px] rounded-[22px] text-otherWhite">
           Get Started
         </button>
-
         <div className="ham lg:hidden">{hamLogo()}</div>
       </nav>
+      <section>
+        <div className="illustration-logo ">{illustrationLogo()}</div>
+        <div className="first-section-text">
+          <h2>Bring everyone together to build better products.</h2>
+          <p>
+            Manage makes it simple for software teams to plan day-to-day tasks
+            while keeping the larger team goals in view.
+          </p>
+          <button className="started lg:flex items-center hover:opacity-50 justify-center hidden lg:block bg-otherOrange font-bold text-[13px] leading-[18.99px] w-[137px] h-[44px] rounded-[22px] text-otherWhite">
+            Get Started
+          </button>
+        </div>
+      </section>
     </>
   );
 }
